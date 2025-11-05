bug in fsky covariance

curlicast/covariance_calculator.py
@@ -12,19 +12,22 @@ class CovarianceCalculatorFsky(object):
         self.factor_modecount = 1/((2*self.leff+1)*self.dell*self.fsky)
 
     def get_covar(self, cl_matrix, nl_vector, i1, i2, j1, j2):
-        cl_i1_j1 = cl_matrix[i1, j1]
+        cl_i1_j1 = cl_matrix[i1, j1].copy()
         if i1 == j1:
             cl_i1_j1 += nl_vector[i1]
         cl_i1_j1 = self.b.bin_cell(cl_i1_j1)
-        cl_i1_j2 = cl_matrix[i1, j2]
+
+        cl_i1_j2 = cl_matrix[i1, j2].copy()
         if i1 == j2:
             cl_i1_j2 += nl_vector[i1]
         cl_i1_j2 = self.b.bin_cell(cl_i1_j2)
-        cl_i2_j1 = cl_matrix[i2, j1]
+
+        cl_i2_j1 = cl_matrix[i2, j1].copy()
         if i2 == j1:
             cl_i2_j1 += nl_vector[i2]
         cl_i2_j1 = self.b.bin_cell(cl_i2_j1)
-        cl_i2_j2 = cl_matrix[i2, j2]
+
+        cl_i2_j2 = cl_matrix[i2, j2].copy()
         if i2 == j2:
             cl_i2_j2 += nl_vector[i2]
         cl_i2_j2 = self.b.bin_cell(cl_i2_j2)
@@ -157,13 +160,13 @@ class CovarianceCalculatorMask(object):
         fsky = self.fsky
 
         # Get signal covariance
-        cl = cl_matrix[i1, j1]
+        cl = cl_matrix[i1, j1].copy()
         psl_i1_j1 = w.couple_cell(np.array([cl0, cl0, cl0, cl]))/fsky
-        cl = cl_matrix[i1, j2]
+        cl = cl_matrix[i1, j2].copy()
         psl_i1_j2 = w.couple_cell(np.array([cl0, cl0, cl0, cl]))/fsky
-        cl = cl_matrix[i2, j1]
+        cl = cl_matrix[i2, j1].copy()
         psl_i2_j1 = w.couple_cell(np.array([cl0, cl0, cl0, cl]))/fsky
-        cl = cl_matrix[i2, j2]
+        cl = cl_matrix[i2, j2].copy()
         psl_i2_j2 = w.couple_cell(np.array([cl0, cl0, cl0, cl]))/fsky
         cw_ss = self.get_cw('ss')
         cov_ss = nmt.gaussian_covariance(cw_ss, 2, 2, 2, 2,

test.py
@@ -41,8 +41,7 @@ class NoiseGenSO(object):
         nl[:, 2:] = nl_from2
         return nl
 
-
-ng = NoiseGenSO()
+ng = NoiseGenSO(n_tube_LF=1, n_tube_MF=9, n_tube_UHF=5)
 
 print("Generating data")
 dg = cct.DataGeneratorPlawFG(config_global=config_global,
@@ -57,6 +56,7 @@ print("Running compsep")
 cs = cct.CompSep('params_test.yml')
 cs.run()
 
+
 print("Analysing chains")
 d = np.load("blah/emcee.npz")
 nwalkers, nsamples, npar = d['chain'].shape
@@ -64,4 +64,4 @@ chain = d['chain'][:, 300:, :].reshape([-1, npar]).T
 for n, c in zip(d['names'], chain):
     mean = np.mean(c)
     std = np.std(c)
-    print(f'{n} = %.3lf +- %.3lf' % (mean, std))
+    print(f'{n} = %.5lf +- %.5lf' % (mean, std))